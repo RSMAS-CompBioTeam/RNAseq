4e8204ebafb320e2ab31d03ead91a9378bf71984
Noah Rose <noahhrose@gmail.com>
2018-05-14 15:00:40 -0400

    updated star align script

--- getReads.sh
+++ getReads.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
-#~/RSMAS-CompBioTeam/RNAseq/sradump.sh
-#purpose: acquire raw RNAseq reads from Sequence Read Archive
+#purpose: acquire raw RNAseq reads from Dropbox
+
+wget https://www.dropbox.com/sh/enrzbdvvu3kluqe/AAAZJbGkPFob2tzsuFsDcToya?dl=1 -O taiwan_fq.zip
+unzip taiwan_fq.zip
